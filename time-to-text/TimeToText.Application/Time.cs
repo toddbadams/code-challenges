dd59e7421916c8fc0736ed56695f182b41e7762c
Todd B. Adams <toddbadams@msn.com>
2020-02-19 15:36:11 +0000

    updates

---
+++ time-to-text/TimeToText.Application/Time.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace TimeToSpeech.Application
+{
+    public class Time
+    {
+        private readonly string _time;
+        private readonly IDictionary<string, string> _specialTimes = new Dictionary<string, string>
+        {
+            { "0000", Midnight}, { "2400", Midnight}, { "1200", Noon }
+        };
+
+        private const string InvalidMessage = "Invalid format, should be [hh][mm] or [hh]:[mm]";
+        private const string Midnight = "Midnight";
+        private const string Noon = "Noon";
+        private const string OClock = "o'clock";
+
+
+        public Time(string time)
+        {
+            _time = ConvertToBasicFormat(time);
+        }
+
+        public override string ToString()
+        {
+            if (_specialTimes.Keys.Contains(_time)) return _specialTimes[_time];
+            if (!IsValidBasicFormat(_time) || 
+                !IsValidHours(_time) || 
+                !IsValidMinutes(_time)) return InvalidMessage;
+
+            var hours = int.Parse(_time.Substring(0, 2));
+            var minutes = int.Parse(_time.Substring(2, 2));
+            if (minutes > 59 || hours > 23) return InvalidMessage;
+
+            return minutes == 0
+                ? $"{_hours} {OClock}"
+                : $"{_minutes} {new Hours(minutes > 30 ? hours + 1 : hours).ToString().ToLowerInvariant()}";
+        }
+
+        private Hours _hours => new Hours(int.Parse(_time.Substring(0, 2)));
+        private Minutes _minutes => new Minutes(int.Parse(_time.Substring(2, 2)));
+
+        private static bool IsValidBasicFormat(string time) =>
+            time != null && time.Length == 4 && time.All(char.IsDigit);
+
+        private Func<string, bool> IsValidHours => (time) => int.Parse(_time.Substring(0, 2)) < 24;
+        private Func<string, bool> IsValidMinutes => (time) => int.Parse(_time.Substring(2, 2)) < 60;
+
+        private static string ConvertToBasicFormat(string time)
+        {
+            var result = time?.Replace(":", "");
+            return result != null && result.Length == 3 ? $"0{result}" : result ?? string.Empty;
+        }
+    }
+}
--- time-to-text/TimeToText.Application/WrittenTimeProcessor.cs
+++ time-to-text/TimeToText.Application/WrittenTimeProcessor.cs
@@ -1,20 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace TimeToSpeech.Application
+﻿namespace TimeToSpeech.Application
 {
     public class WrittenTimeProcessor
     {
-        private readonly IDictionary<string, string> _specialTimes = new Dictionary<string, string>
-        {
-            { "0000", Midnight}, { "2400", Midnight}, { "1200", Noon }
-        };
-
-        private const string InvalidMessage = "Invalid format, should be [hh][mm] or [hh]:[mm]";
-        private const string Midnight = "Midnight";
-        private const string Noon = "Noon";
-        private const string OClock = "o'clock";
-
         /// <summary>
         /// Present time in a more "Human Friendly" way
         /// </summary>
@@ -22,27 +9,7 @@ namespace TimeToSpeech.Application
         /// <returns>Human friendly time</returns>
         public string Process(string time)
         {
-            time = ConvertToBasicFormat(time);
-            if (!IsValidBasicFormat(time)) return InvalidMessage;
-
-            if (_specialTimes.Keys.Contains(time)) return _specialTimes[time];
-
-            var hours = int.Parse(time.Substring(0, 2));
-            var minutes = int.Parse(time.Substring(2, 2));
-            if (minutes > 59 || hours > 23) return InvalidMessage;
-
-            return minutes == 0
-                ? $"{new Hours(hours)} {OClock}"
-                : $"{new Minutes(minutes)} {new Hours(minutes > 30 ? hours + 1 : hours).ToString().ToLowerInvariant()}";
+            return new Time(time).ToString();
         }
-
-        private static string ConvertToBasicFormat(string time)
-        {
-            var result = time?.Replace(":", "");
-            return result != null && result.Length == 3 ? $"0{result}" : result ?? string.Empty;
-        }
-
-        private static bool IsValidBasicFormat(string time) =>
-            time != null && time.Length == 4 && time.All(char.IsDigit);
     }
 }
