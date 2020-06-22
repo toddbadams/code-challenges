16e4a5f7d34710ab1c7f8060382076ce0e52e6c6
Todd B. Adams <toddbadams@msn.com>
2020-06-22 13:04:00 +0100

    updates

--- iot/src/RefrigeratedTruck/Application/Configuration/AzureMapsConfig.cs
+++ iot/src/RefrigeratedTruck/Application/Configuration/AzureMapsConfig.cs
@@ -2,6 +2,6 @@
 {
     public static class AzureMapsConfig
     {
-        public static string AzureMapsKey = "ktOT45A5Oarl3D3ouINTuvyleezwzi-6cL5dfgOJmIk";
+        public static string AzureMapsKey = "[tbd]";
     }
 }
